125744f916bbeb53ea68b815fdb5fa027e1d23f0
another-dj <alexis_forestier@hotmail.com>
2019-11-11 15:39:03 +0000

    not finished yet

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,18 +1,31 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort();
+    this.length = this.items.length;
   }
 
   get(pos) {
-    // ...
+    let position = this.items[pos];
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return position;
+    }
   }
 
   max() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      const maximum = this.items;
+      return Math.max(...maximum);
+    }
   }
 
   min() {
