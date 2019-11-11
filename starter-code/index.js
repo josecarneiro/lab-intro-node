9e4e60d518c56a5bd83c58c7343e1008b3dd006a
Diogo Marques <diogo@fontainhas.com>
2019-11-11 10:55:16 +0000

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,30 +1,42 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+
+    this.length = this.items.length;
   }
 
   get(pos) {
-    // ...
+    return this.items[pos];
   }
 
   max() {
-    // ...
+    if(!this.length > 0) throw new Error("EmptySortedList");
+    return Math.max(...this.items);
   }
 
   min() {
-    // ...
+    if(!this.length > 0) throw new Error("EmptySortedList");
+    return Math.min(...this.items);
   }
 
   average() {
-    // ...
+    if(!this.length > 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
   }
 
   sum() {
-    // ...
+    if(!this.length > 0) return 0;
+    return this.items.reduce((acc, val) => {
+      return acc + val;
+    });
   }
 }
 
