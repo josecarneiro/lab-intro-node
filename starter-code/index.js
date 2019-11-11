ad0b5081b855a6dc75f037fe6379baecfaf14b87
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-11 15:27:21 +0000

    lab done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,30 +1,58 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
   }
 
   get(pos) {
-    // ...
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] > this.items.length) {
+        throw new Error('outOfBounds');
+      }
+      return this.items[pos];
+    }
   }
 
   max() {
-    // ...
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
   }
 
   min() {
-    // ...
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
   }
 
   sum() {
-    // ...
+    if (this.items.length < 1) {
+      return 0;
+    } else {
+      return this.items.reduce(function (acumulador, valorAtual, indice, array) {
+        return acumulador + valorAtual;
+      }, 0);
+    }
   }
 
   average() {
-    // ...
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList')
+    } else {
+      return this.sum() / this.items.length
+    }
   }
 }
 
