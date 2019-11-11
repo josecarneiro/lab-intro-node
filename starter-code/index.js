df17e74acced5296c655c445186950c329950f8a
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-11-11 15:42:42 +0000

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -6,17 +6,16 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
+    this.length = this.items.length;
     this.items.sort((a, b) => {
       return a - b;
     });
-    this.length = this.items.length;
   }
 
   get(pos) {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i] > this.items.length) {
-        throw new Error('outOfBounds');
-      }
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+    } else {
       return this.items[pos];
     }
   }
