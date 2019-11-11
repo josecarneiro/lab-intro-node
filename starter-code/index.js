f7e366e6f417e816b5a7556f50807fc265882c9f
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-11-11 15:33:51 +0000

    updated

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,31 +1,56 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   get(pos) {
-    // ...
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
   }
 
   max() {
-    // ...
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+      return Math.max(...this.items);
   }
 
   min() {
-    // ...
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
   }
 
   sum() {
-    // ...
+    let total = 0;
+    if (this.length === 0){
+      return 0
+    } else {
+      for (let i = 0; i < this.length; i++){
+      total += this.items[i]
+    }
+    return total
   }
+  }
+  
 
   average() {
-    // ...
-  }
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+      return this.sum()/ this.length;
+    }
 }
 
 module.exports = SortedList;
