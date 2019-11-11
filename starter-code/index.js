11d454d21d50af1904259a8b47a07d35977dea14
FilipeMts <fxmatos@mail.com>
2019-11-11 16:53:27 +0000

    Finished lab

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,30 +1,69 @@
 class SortedList {
-  constructor() {
+  constructor(items, length) {
     // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
     // ...
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a -b;
+    });
+    this.length = this.items.length;
   }
 
   get(pos) {
     // ...
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+    }
+    else {                  
+      return this.items[pos];
+    }    
   }
 
   max() {
     // ...
+    if (this.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.max(...this.items)
+    }
   }
 
   min() {
     // ...
+    if (this.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return Math.min(...this.items)
+    }
   }
 
   sum() {
     // ...
+    if (this.length < 1) {
+      return 0;
+    }
+    else {
+      return this.items.reduce((a, b) => {
+        return a + b;
+      }, 0);
+    }
   }
 
   average() {
-    // ...
+    // ...    
+    if (this.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    else {
+      return this.sum() / this.items.length
+    }
   }
 }
 
