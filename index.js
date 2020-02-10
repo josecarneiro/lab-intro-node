56692536fe8308755e6ef30403e560e60cd103ea
David Felix <davidfelix1997@hotmail.com>
2020-02-10 13:56:04 +0000

    Finished

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 class SortedList {
   constructor() {
     // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
-  add(item) {
+  add(x) {
     // ...
-  }
 
+    this.items.push(x);
+    this.length++;
+    this.items.sort((a, b) => a - b);
+  }
   load(pos) {
     // ...
+    if (this.length < pos) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
   }
 
   max() {
     // ...
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length - 1];
+    }
   }
 
   min() {
     // ...
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
   }
 
   sum() {
     // ...
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      sum += this.items[i];
+    }
+    return sum;
   }
 
   avg() {
-    // ...
+    let sum = 0;
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      for (let i = 0; i < this.items.length; i++) {
+        sum += this.items[i];
+      }
+      return (sum /= this.items.length);
+    }
   }
 }
 
