6e5ff59366b0b7cdc9d904b2a47f090169108d24
Joana Cruz <joanamartadacruz@gmail.com>
2020-02-10 12:16:36 +0000

    done

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    let i = 0;
+    while(this.items[i] <= item) i++;
+    this.items.splice(i, 0, item);
+    this.length++;
   }
 
   load(pos) {
-    // ...
+    if(pos <= this.length - 1){
+      return this.items[pos];
+    }
+    else{
+      throw new Error('OutOfBounds');
+    }
   }
 
   max() {
-    // ...
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.max(...this.items);
+    } 
   }
 
   min() {
-    // ...
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return Math.min(...this.items);
+    } 
   }
 
   sum() {
-    // ...
+    return this.items.reduce((x, acc) => acc + x, 0);
   }
 
   avg() {
-    // ...
+    if(this.length === 0) throw new Error('EmptySortedList');
+    else return this.sum()/this.length;
   }
 }
 
