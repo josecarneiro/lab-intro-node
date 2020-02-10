60b1db372abf89856d7eaa7a22169744c661fad2
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-02-10 12:30:57 +0000

    Lab completed.

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
 class SortedList {
-  constructor() {
-    // ...
+  constructor(items) {
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   load(pos) {
-    // ...
+    if (this.items[pos] !== undefined) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+
+    //if (this.items[pos] !== undefined) {this.items[pos]} else {'OutOfBounds'}
   }
 
   max() {
-    // ...
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.items.length - 1];
+    }
   }
 
   min() {
-    // ...
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
   }
 
   sum() {
-    // ...
+    var add = 0;
+    for (let i of this.items) {
+      add += i;
+    }
+    return add;
   }
 
   avg() {
-    // ...
+    if (this.items == '') {
+      throw new Error('EmptySortedList');
+    } else {
+      var add = 0;
+      for (let i of this.items) {
+        add += i;
+      }
+
+      return add / this.items.length;
+    }
   }
 }
 
