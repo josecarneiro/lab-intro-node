ed17199714903d0af16e89033149e056d4fa3e49
Felipe Lima <felipenplima@gmail.com>
2020-02-10 12:42:07 +0000

    bonuses undone

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 class SortedList {
-  constructor() {
-    // ...
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    return (this.length = this.items.length);
   }
 
   load(pos) {
-    // ...
+    if (this.length > pos) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
   }
 
   max() {
-    // ...
+    if (this.items.length !== 0) {
+      return Math.max(...this.items); //... chama do array
+    } else {
+      throw new Error('EmptySortedList');
+    }
   }
 
   min() {
-    // ...
+    {
+      if (this.items.length !== 0) {
+        return Math.min(...this.items); //... chama do array
+      } else {
+        throw new Error('EmptySortedList');
+      }
+    }
   }
 
   sum() {
