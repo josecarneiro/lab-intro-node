1956b0e7218765b0317e632cd2a1f99c0ad278b0
unknown <ricardotomaz027@gmail.com>
2020-02-10 13:44:16 +0000

    Solved lab

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
   }
 
   load(pos) {
-    // ...
+    if (this.items.length > pos) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error('OutOfBounds');
+    }
   }
 
   max() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
   }
 
   min() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
   }
 
   sum() {
-    // ...
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer);
+    }
   }
 
   avg() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.sum() / this.items.length;
+    }
   }
 }
 
